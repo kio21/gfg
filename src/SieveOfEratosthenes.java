c4dc646987c60646b9f0e06cf825655df62a3696
ikravchenko <ikravchenko@smartru.com>
2023-04-11 21:38:06 +0300

    init sieve of eratosthenes

---
+++ src/SieveOfEratosthenes.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a number N.
+ * Calculate the prime numbers up to N using Sieve of Eratosthenes.
+ */
+public class SieveOfEratosthenes {
+    static ArrayList<Integer> sieveOfEratosthenes(int N) {
+        ArrayList<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
